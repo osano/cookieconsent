d3a8a7fdb9153002f09cb77160da121dc8d316aa
Filip Oščádal <filip@mxd.cz>
2015-09-19 10:27:48 +0200

    changes to the HTML code - links open in a new window

--- cookieconsent.js
+++ cookieconsent.js
@@ -239,8 +239,7 @@
         '<div class="cc_banner-wrapper {{containerClasses}}">',
         '<div class="cc_banner cc_container cc_container--open">',
         '<a href="#null" data-cc-event="click:dismiss" target="_blank" class="cc_btn cc_btn_accept_all">{{options.dismiss}}</a>',
-        '<p class="cc_message">{{options.message}} <a data-cc-if="options.link" class="cc_more_info" href="{{options.link || "#null"}}">{{options.learnMore}}</a></p>',
-        '<a class="cc_logo" target="_blank" href="http://silktide.com/cookieconsent">Cookie Consent plugin for the EU cookie law</a>',
+        '<p class="cc_message">{{options.message}} <a data-cc-if="options.link" target="_blank" class="cc_more_info" href="{{options.link || "#null"}}">{{options.learnMore}}</a></p>',
         '</div>',
         '</div>'
       ]
