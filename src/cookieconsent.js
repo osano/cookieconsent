be4fb8d9e0f20f8b714b1a262a8106a07614e285
Lorenzo Marcon <lorenzo.marcon@gmail.com>
2017-07-23 15:13:39 +0200

    added rel noopener noreferrer nofollow to external link

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -222,7 +222,7 @@
       elements: {
         header: '<span class="cc-header">{{header}}</span>&nbsp;',
         message: '<span id="cookieconsent:desc" class="cc-message">{{message}}</span>',
-        messagelink: '<span id="cookieconsent:desc" class="cc-message">{{message}} <a aria-label="learn more about cookies" role=button tabindex="0" class="cc-link" href="{{href}}" target="_blank">{{link}}</a></span>',
+        messagelink: '<span id="cookieconsent:desc" class="cc-message">{{message}} <a aria-label="learn more about cookies" role=button tabindex="0" class="cc-link" href="{{href}}" rel="noopener noreferrer nofollow" target="_blank">{{link}}</a></span>',
         dismiss: '<a aria-label="dismiss cookie message" role=button tabindex="0" class="cc-btn cc-dismiss">{{dismiss}}</a>',
         allow: '<a aria-label="allow cookies" role=button tabindex="0"  class="cc-btn cc-allow">{{allow}}</a>',
         deny: '<a aria-label="deny cookies" role=button tabindex="0" class="cc-btn cc-deny">{{deny}}</a>',
