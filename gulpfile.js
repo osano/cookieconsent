4ab98f5c0a36ffdceea917cfd92b698cbd9049d0
Arlo Gilbert <arlogilbert@me.com>
2019-05-23 15:59:44 -0500

    Revert "Merge pull request #542 from b3none/patch-1"
    This reverts commit e2074ae1b808cd6fef9d654fb79b15d7fb593375, reversing
    changes made to b08a33b5d01f6447f37e805f588f4cdcf6e0e6d2.

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const bump = require('gulp-bump');
 const yargs = require('yargs');
 const diff = require('gulp-diff-4');
 
+
 let buildFolder = './build';
 const jsBuildFiles = [
   './src/cookieconsent.js'
@@ -23,52 +24,53 @@ const cssBuildFiles = [
   './src/styles/themes/*.css',
 ];
 
-gulp.task('cleanup:begin', () => 
-  deleteDirs([buildFolder]);
-);
 
-gulp.task('minify:js', () =>
-  gulp.src(jsBuildFiles)            // get files
+gulp.task('cleanup:begin', function () {
+  return deleteDirs([buildFolder]);
+});
+
+gulp.task('minify:js', function () {
+  return gulp.src(jsBuildFiles)            // get files
     .pipe(minifyJS())                      // minify them
     .pipe(concat('cookieconsent.min.js'))  // combine them
     .pipe(gulp.dest(buildFolder));          // save under a new name
-);
+});
 
-gulp.task('minify:css', () => 
-  gulp.src(cssBuildFiles)            // get files
+gulp.task('minify:css', function () {
+  return gulp.src(cssBuildFiles)            // get files
     .pipe(autoprefixer({browsers: ['IE 10', 'last 2 versions']}))
     .pipe(minifyCSS())                      // minify them
     .pipe(concat('cookieconsent.min.css'))  // combine them
     .pipe(gulp.dest(buildFolder));          // save under a new name
-);
+});
 
-gulp.task('bump', () => 
-  gulp.src(['./bower.json', './package.json'])
-      .pipe(bump({'version': yargs.argv.tag}))
-      .pipe(gulp.dest('./'))
-);
+gulp.task('bump', function() {
+  return gulp.src(['./bower.json', './package.json'])
+             .pipe(bump({'version': yargs.argv.tag}))
+             .pipe(gulp.dest('./'))
+});
 
 gulp.task('build', gulp.series('cleanup:begin', 'minify:js', 'minify:css'));
 
-gulp.task('verify', () => {
+gulp.task('verify', function() {
   buildFolder = "./build-verify";
   return new Promise(gulp.series('cleanup:begin', 'minify:js', 'minify:css', 'verify:diff'));
 });
 
-gulp.task('verify:diff', () =>
-  gulp.src('./build/*')
-      .pipe(diff('./build-verify'))
-      .pipe(diff.reporter({ fail: true }));
-);
+gulp.task('verify:diff', function() {
+  return gulp.src('./build/*')
+             .pipe(diff('./build-verify'))
+             .pipe(diff.reporter({ fail: true }));
+});
 
-gulp.task('build:release', () => {
-  if (yargs.argv.tag === undefined) {
+gulp.task('build:release', function() {
+  if (yargs.argv.tag===undefined) {
     throw "A version number (e.g. 3.0.1) is required to build a release of cookieconsent"
   }
 
   return new Promise(gulp.series('build', 'bump'));
 });
 
-gulp.task('watch', () => {
+gulp.task('watch', function() {
   gulp.watch(cssBuildFiles.concat(jsBuildFiles), ['build']);
 });
