1b332da8905995e9641bb68756d58aae1abb4468
Eric Hodonsky <relic.melex@users.noreply.github.com>
2019-07-25 13:53:07 -0700

    Updates handeling of expireDays to 0 being useful

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -23,7 +23,7 @@
 
     setCookie: function(name, value, expiryDays, domain, path, secure) {
       const exdate = new Date()
-      exdate.setHours(exdate.getHours() + ((expiryDays || 365) * 24))
+      exdate.setHours(exdate.getHours() + ((typeof expiryDays !== "number"  ? 365 : expiryDays ) * 24))
       document.cookie = name + '=' + value +
                         ';expires=' + exdate.toUTCString() +
                         ';path=' + (path || '/') +
