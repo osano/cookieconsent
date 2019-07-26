6d62ab357c9fe2b43ff692afba00f55d6c0751e4
Andreas Kar <andreas.kar@gmx.at>
2019-07-26 04:11:05 +0200

    changed forceFloat parameter to mobileForceFloat

--- src/cookieconsent.js
+++ src/cookieconsent.js
@@ -385,7 +385,7 @@
       autoAttach: true,
 	  
 	  // set value if floating layout should be forced for mobile devices
-	  forceFloating: true,
+	  mobileForceFloat: true,
 
       // simple whitelist/blacklist for pages. specify page by:
       //   - using a string : '/index.html'           (matches '/index.html' exactly) OR
@@ -734,7 +734,7 @@
           ? 'banner'
           : 'floating';
 
-      if (util.isMobile() && opts.forceFloating) {
+      if (util.isMobile() && opts.mobileForceFloat) {
         positionStyle = 'floating';
       }
 
