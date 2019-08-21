19c6e50378efd49eff5b152515410b8feed87947
Eric Hodonsky <e@osano.com>
2019-08-21 09:31:04 -0700

    Fixes babel plugins breaking builds

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,25 @@
 module.exports = proc => {
   const isTest = proc.env("test")
+  const plugins = [
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        "legacy": true
+      }
+    ],
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-flow-strip-types",
+    "@babel/plugin-transform-runtime"
+  ]
+  if ( isTest ){
+    plugins.push( "transform-es2015-modules-commonjs" )
+  }
   return {
     "presets": [
       [ "@babel/preset-env", { targets: { node: "current" } } ]
     ],
-    "plugins": [
-      [
-        "@babel/plugin-proposal-decorators",
-        {
-          "legacy": true
-        }
-      ],
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-object-rest-spread",
-      "@babel/plugin-transform-flow-strip-types",
-      "@babel/plugin-transform-runtime",
-      ( isTest ? "transform-es2015-modules-commonjs" : "" )
-    ],
+    plugins,
     "ignore": [
       "node_modules/**/*"
     ],
