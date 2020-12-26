99fa45dd65db84484579c72960c9ef152e4834a3
Scott Willeke <scott@willeke.com>
2020-12-25 20:55:52 -0800

    fix\!: module-only + typescript types; works on my machine

---
+++ types/index.d.ts
@@ -0,0 +1,33 @@
+declare module "cookieconsent" {
+  export type ColorString = string
+  export type ConsentOptions = {
+    enabled?: boolean,
+    palette: {
+      popup?: {
+        background?: ColorString,
+        text?: ColorString
+      },
+      button?: {
+        background?: ColorString,
+        text?: ColorString,
+        border?: ColorString
+      }
+    },
+    content?: {
+      header? : string,
+      message?: string,
+      dismiss?: string,
+      allow?  : string,
+      deny?   : string,
+      link?   : string,
+      href?   : string,
+      close?  : string,
+      target? : string,
+      policy? : string,
+  
+    }
+  }
+  export default class CookieConsent {
+    constructor (options: ConsentOptions)
+  }
+}
