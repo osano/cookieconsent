c053789d8a585683420d2818d6edad46d7d033be
scott willeke <scott@willeke.com>
2021-11-02 21:15:55 -0700

    feat: Example and TypeScript definitions for events
    Subscribe to an event with code like `cc.on( "statusChanged", ( ...args ) => console.log( args ) )`.
    
    The available events are initialized, error, popupOpened, popupClosed, revokeChoice, and statusChanged. See example at `examples-new/events/`.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,33 +1,46 @@
 declare module "@activescott/cookieconsent" {
   export type ColorString = string
   export type ConsentOptions = {
-    enabled?: boolean,
+    enabled?: boolean
     palette: {
       popup?: {
-        background?: ColorString,
+        background?: ColorString
         text?: ColorString
-      },
+      }
       button?: {
-        background?: ColorString,
-        text?: ColorString,
+        background?: ColorString
+        text?: ColorString
         border?: ColorString
       }
-    },
+    }
     content?: {
-      header? : string,
-      message?: string,
-      dismiss?: string,
-      allow?  : string,
-      deny?   : string,
-      link?   : string,
-      href?   : string,
-      close?  : string,
-      target? : string,
-      policy? : string,
-  
+      header?: string
+      message?: string
+      dismiss?: string
+      allow?: string
+      deny?: string
+      link?: string
+      href?: string
+      close?: string
+      target?: string
+      policy?: string
     }
   }
+
+  type statusChangedEventHandler = (
+    cookieName: string,
+    status: string,
+    chosenBefore: boolean
+  ) => void
+
+  type genericEventHandler = (...args: any) => void
+
   export default class CookieConsent {
-    constructor (options: ConsentOptions)
+    constructor(options: ConsentOptions)
+    on(eventName: string, eventHandler: genericEventHandler): void
+    on(
+      eventName: "statusChanged",
+      eventHandler: statusChangedEventHandler
+    ): void
   }
 }
